0feba9ff126899fa9dd21e820b9e82f14bbae774
taylornelms15 <taylornelms15@gmail.com>
2019-11-06 12:19:22 -0500

    initial file commit for lzw hw functions

---
+++ src/lzw_hw.c
@@ -0,0 +1,19 @@
+/**
+@file lzw_hw.c
+@author Taylor Nelms
+*/
+
+#include "lzw_hw.h"
+
+
+
+
+
+int lzwCompress(const uint8_t* input, int numElements, uint8_t* output){
+
+
+
+    return 0;
+}
+
+
---
+++ src/lzw_hw.h
@@ -0,0 +1,24 @@
+/**
+@file lzw_hw.h
+@author Taylor Nelms
+*/
+
+#ifndef LZW_HW_H
+#define LZW_HW_H
+
+#include "common.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
