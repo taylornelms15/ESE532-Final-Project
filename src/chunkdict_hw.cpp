55252946d90aec080c069871272e822e954d0172
Taylor Nelms <taylornelms15@gmail.com>
2019-11-15 13:21:44 -0500

    chunkdict progress

--- src/chunkdict_hw.cpp
+++ src/chunkdict_hw.cpp
@@ -3,21 +3,10 @@
  * @author Taylor Nelms
  */
 
-
-
-
-#include "chunkdict.h"
+#include "chunkdict_hw.h"
 #include "ap_uint.h"
 
-#define INDEXBITS 17
-#define HASHROWS 1024
-#define HASHBITS 10
-#define SHANOTFOUND 0x1FFFF
-
 static uint32_t currentIndex = 0;
-
-
-//SHA256_BLOCK_SIZE - number of bytes in our SHA block (32)
 
 // Table in DRAM wants to be storing something like 200k "rows"
 // Each row is holding a pair of "ShaValue, Index"
@@ -27,49 +16,142 @@ static uint32_t currentIndex = 0;
 // Example: let's say we have 128 potential rows, each storing an 8-bit key and a 7-bit index
 // If hash key down to 6 bits (64 buckets), ideal would be 2 rows per bucket
 // If our hashed value were "3", would pull rows at indices 6 and 7
-//
-// On-chip memory... actually need not exist. It's just a hash function.
-// We can define the depth we need (call it 2?), and then
 
 
+/**
+ * Makes a hash of the incoming SHA256 value
+ * Just does a series of XOR operations; assuming reasonable spread of the SHA256 values (roughly equal entropy for each bit)
+ * If we like 16 as a number of hash bits, this equation can be simplified to do byte-level XOR operations, rather than bit-level (will undo a lot of compute load)
+ */
+ap_uint<HASHBITS> hashShaKey(const uint8_t input[SHA256_SIZE]){
+    #pragma HLS inline
 
-// Hash table - up to 200k entries in it total (less than 2^18, I'm even willing to assume 2^17 for bit purposes)
-// Each one is 256 + <hashbits> long
-// But I guess what we really need is just a way to store a bunch of indexes.
-// Because table never cleared, no real worry about "not starting from 0"
-// Once we find our indexes, can check
-// One BRAM is 512 rows each storing 2ish row indexes
-// Assuming even hash buckets (not unreasonable, given SHA input): for each
+    ap_uint<HASHBITS> retval = 0;
+    uint8_t currentHashBit  = 0;
+
+    for(uint8_t i = 0; i < SHA256_SIZE; i++){
+        #pragma HLS unroll
+        uint8_t currentShaByte = input[i];
+        for(uint8_t j = 0; j < 8; j++){
+            ap_uint<HASHBITS> valToXor = 1 << currentHashBit;
+            if (currentShaByte & (1 << j)){
+                //do nothing
+            }//if 1
+            else{
+                valToXor = 0;
+            }//else 0
+            retval ^= valToXor;        
+
+            currentHashBit = (currentHashBit + 1) % HASHBITS;
+        }//for j (per sha byte's bit)
+    }//for i (per sha byte)
+
+    return retval;
+    
+
+}//hashShaKey
 
 
-void storeNewValue(const BYTE newVal[SHA256_BLOCK_SIZE]){
+/**
+ * Gets the address offset on the DRAM side to look for our entries
+ */
+uint32_t getDramOffset(ap_uint<HASHBITS> hashVal){
+    uint32_t rowNum = hashVal * ((uint32_t) NUM_ENTRIES_PER_HASH_VALUE);
+    return rowNum * BYTES_PER_ROW;
+}//getDramOffset
 
-    uint8_t storeBuffer[SHA256_BLOCK_SIZE + 4];//stores the SHA value, plus 4 bytes for the current index
+
+void storeNewValue(const uint8_t newVal[SHA256_SIZE], uint8_t* tableLocation, ap_uint<HASHBITS> key, uint8_t candidates[DRAM_PULL_SIZE]){
+
+    uint8_t storeBuffer[BYTES_PER_ROW];//stores the SHA value, plus 4 bytes for the current index
     memcpy(storeBuffer, newVal, SHA256_BLOCK_SIZE);
     memcpy(storeBuffer + SHA256_BLOCK_SIZE, &currentIndex, 4);
+
+    uint32_t offset = getDramOffset(key);
+
+    int rowOffset = -1;
+    for (int i = 0; i < NUM_ENTRIES_PER_HASH_VALUE; i += BYTES_PER_ROW){
+        #pragma HLS unroll
+        uint32_t candIndex;
+        uint8_t* indexPortion = candidates[i + SHA256_SIZE];
+        memcpy(&candIndex, indexPortion, sizeof(uint32_t));
+        if (candIndex >= SHANOTFOUND){
+            rowOffset = i;
+            break;
+        }//found a free part of the table
+    }//for each candidate
+    if (rowOffset < 0){
+        //TODO: handle a hash overflow here!!!!!
+    }//if hash overflow
+
+    uint32_t storageOffset = offset + rowOffset;
+    memcpy(tableLocation + storageOffset, storeBuffer, BYTES_PER_ROW);
 
     currentIndex++;
 }//storeNewValue
 
+/**
+ * Pulls all possible records of our hash-index pair from DRAM memory
+ */
+void pullRecordsFromTable(const uint8_t* tableLocation, uint8_t* destination, ap_uint<HASHBITS> key){
+    uint32_t offset = getDramOffset(key);
 
-int indexForShaValue_HW(const BYTE input[SHA256_BLOCK_SIZE]){
+    memcpy(destination, tableLocation + offset, DRAM_PULL_SIZE);
+}//pullRecordsFromTable
+
+/**
+ * @return 1 if buffers equal, 0 otherwise
+ */
+int shaRecordsEqual(uint8_t record1[SHA256_SIZE], uint8_t record2[SHA256_SIZE]){
+    #pragma HLS inline
+    int retval = 1;
+    for(int i = 0; i < SHA256_SIZE; i++){
+        #pragma HLS unroll
+        if (record1[i] != record2[i]){
+            retval = 0;
+        }//if
+    }//for
+
+    return retval
+}//shaRecordsEqual
+
+int indexForShaValue_HW(const uint8_t input[SHA256_SIZE], uint8_t* tableLocation){
+    ap_uint<HASHBITS> key = hashShaKey(input);
+    uint8_t candidates[DRAM_PULL_SIZE];
+
+    pullRecordsFromTable(tableLocation, candidates, key);
+
+
+    uint32_t indexVal = SHANOTFOUND;
+    for (int i = 0; i < NUM_ENTRIES_PER_HASH_VALUE; i += BYTES_PER_ROW){
+        #pragma HLS unroll
+        uint8_t* shaPortion = candidates[i];
+        uint8_t* indexPortion = candidates[i + SHA256_SIZE];
+        if (shaRecordsEqual(input, shaPortion)){
+            memcpy(&indexVal, indexPortion, sizeof(uint32_t));
+        }//if
+    }//for each candidate
+
+    if (indexVal >= SHANOTFOUND){//should cover weird case where all-ones triggers weird stuff
+        storeNewValue(input, tableLocation, key, candidates);
+
+        return -1;
+    }//if
+    else{
+        return indexVal;
+    }//else
 
 
 
+    return 0;
 }//indexForShaValue_HW
 
-void resetTable_HW(){
+/**
+ * Note: this should NOT be a hardware function!
+ */
+void resetTable_HW(uint8_t* tableLocation){
 
-}//resetTable_HW
-
-
-
-
-
-
-
-
-
+}//resetTable
 
 
 
--- src/chunkdict_hw.h
+++ src/chunkdict_hw.h
@@ -5,12 +5,28 @@
 #ifndef CHUNKDICT_H
 #define CHUNKDICT_H
 
-#include "sha_256.h"
 #include "common.h"
+#define SHA256_SIZE 32//32-byte digest
+#define BYTES_PER_ROW (SHA256_SIZE + 4)
+#define INDEXBITS 17
+#define HASHROWS 1024
+#define HASHBITS 16
+#define HASHDEPTH 2
+#define SHANOTFOUND 0x1FFFF
+#define NUM_ENTRIES_PER_HASH_VALUE ((1 << (INDEXBITS - HASHBITS)) * HASHDEPTH)
+#define DRAM_PULL_SIZE (BYTES_PER_ROW * NUM_ENTRIES_PER_HASH_VALUE)
+#define NUMHASHBUCKETS (1 << HASHBITS)
+#define SHA256TABLESIZE (NUMHASHBUCKETs * DRAM_PULL_SIZE)
 
 
-int indexForShaVal(const BYTE input[SHA256_BLOCK_SIZE]);
-void resetTable();
+#pragma SDS data zero_copy(tableLocation[0:SHA256TABLESIZE])
+#pragma SDS data copy(input[0:SHA256_SIZE])
+#pragma SDS data access_pattern(input:RANDOM, tableLocation:RANDOM)
+int indexForShaVal_HW(const uint8_t input[SHA256_SIZE], uint8_t* tableLocation);
+
+
+
+void resetTable_HW(uint8_t* tableLocation);
 
 
 
