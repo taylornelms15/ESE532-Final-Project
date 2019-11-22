a3d36c29835b856259a374a30d1149b42ec5bc5a
TAYLOR NELMS <tknelms@park.key.seas.upenn.edu>
2019-11-22 12:31:49 -0500

    some functionality for testing chunkdict

--- src/chunkdict.h
+++ src/chunkdict.h
@@ -5,9 +5,17 @@
 #ifndef CHUNKDICT_H
 #define CHUNKDICT_H
 
-#include "sha_256.h"
-#include "common.h"
+//#include "sha_256.h"
+//#include "common.h"
 
+typedef unsigned char BYTE;
+
+#define SHA256_BLOCK_SIZE 32
+
+#define MINSIZE (1 * 1024)
+#define MAXSIZE (6 * 1024)
+#define MAXINPUTFILESIZE (200000000)//200MB
+#define MAX_CHUNK_NUM (MAXINPUTFILESIZE / MINSIZE + 1)//may want to modify where/how this is declared
 
 int indexForShaVal(const BYTE input[SHA256_BLOCK_SIZE]);
 void resetTable();
