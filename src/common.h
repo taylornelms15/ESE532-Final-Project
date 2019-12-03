045cf4bddc0a4c1d412bed812b3fa03230a85bdd
Taylor Nelms <taylornelms15@gmail.com>
2019-12-03 18:27:23 -0500

    last typedef

--- src/common.h
+++ src/common.h
@@ -11,6 +11,7 @@ typedef unsigned int uint32_t;
 typedef int int32_t;
 typedef short int16_t;
 typedef unsigned short uint16_t;
+typedef unsigned long uint64_t;
 
 #define HALF_ENABLE_CPP11_CMATH 0
 
